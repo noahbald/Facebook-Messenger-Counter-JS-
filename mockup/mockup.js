3d05d0a5e3123609456ed64997f26d3fa547823a
Noah Baldwin <nbal0003@student.monash.edu>
2019-03-23 10:03:15 +1100

    Make button and tab onclick functions less dependent on html ids
    - uses 'this' as function argument rather than element id
    - more efficient and maintainable

--- mockup/mockup.js
+++ mockup/mockup.js
@@ -1,18 +1,58 @@
 var dropdown_list = null;
 var self_clicked = true;
 
-function dropdownOnClick(id)
+$(document).ready(function(){
+function dropdownOnClick(self)
 {
-    if (dropdown_list != null)
+    others = document.getElementsByClassName("dropdown");
+    dropdown_list = self.parentElement.getElementsByClassName("dropdown")[0]
+    for (var i=0; i<others.length; i++)
     {
-        if (dropdown_list.id != id)
+        if (others[i] == dropdown_list)
         {
-            dropdown_list.classList.add("hidden");
-            document.getElementById(id).classList.add("hidden");
+            continue;
         }
-        self_clicked = true;
+        others[i].classList.add("hidden");
     }
-    dropdown_list = document.getElementById(id);
+    dropdown_list.classList.toggle("hidden");
+    self_clicked = true;
+}
+
+function dropdown_optionOnClick(self)
+{
+    parent = self.parentElement
+    button = parent.parentElement.getElementsByClassName("button")[0];
+    button.innerHTML = self.innerHTML;
+    others = parent.getElementsByClassName("dropdown_option");
+    for (var i=0; i<others.length; i++)
+    {
+        others[i].classList.remove("selected");
+    }
+    self.classList.add("selected");
+}
+
+function isChrome()
+{
+    var isChromium = window.chrome;
+    var winNav = window.navigator;
+    var vendorName = winNav.vendor;
+    var isOpera = typeof window.opr !== "undefined";
+    var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+    var isIOSChrome = winNav.userAgent.match("CriOS");
+
+    if (isIOSChrome) {
+       return -1;
+    } else if(
+      isChromium !== null &&
+      typeof isChromium !== "undefined" &&
+      vendorName === "Google Inc." &&
+      isOpera === false &&
+      isIEedge === false
+    ) {
+       return True;
+    } else {
+       return False;
+    }
 }
 
 window.onclick = function(e)
@@ -26,7 +66,6 @@ window.onclick = function(e)
         }
         else
         {
-            dropdown_list.classList.toggle("hidden");
             self_clicked = false;
         }
     }
